d48ab9bec1313fb720220fb55eb13c4fb5ccc64e
sacredflower <18224017933@163.com>
2021-02-14 21:32:34 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'webpack performance optimization',
+    themeConfig: {
+        nav: [{ text: 'another page', link: '/hello.html' }],
+        sidebar: ['/', '/hello'],
+    },
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# Webpack性能优化实践
+## 减小打包体积
+## 加快构建速度
+## 优化Web图片
+
+[go to another page](./hello.md)
